4d55c9d83b58e3ec428b5dbf537d6a91edfdaa9b
Skip Morrow <skip@pelorus.org>
2024-09-24 09:58:10 -0400

    curveTest.py

---
+++ curveTest.py
@@ -0,0 +1,24 @@
+from pybricks.pupdevices import Motor
+
+from pybricks.parameters import (
+    Port,
+    Direction,
+    Stop,
+)
+from pybricks.robotics import DriveBase
+
+TIRE_DIAMETER = 56  # mm
+AXLE_TRACK = 103  # distance between the wheels, mm
+
+leftDriveMotor = Motor(Port.E, Direction.COUNTERCLOCKWISE)
+rightDriveMotor = Motor(Port.A)
+robot = DriveBase(
+    leftDriveMotor,
+    rightDriveMotor,
+    TIRE_DIAMETER,
+    AXLE_TRACK,
+)
+
+robot.settings(straight_speed=900, straight_acceleration=300)
+robot.straight(distance=700, then=Stop.NONE)
+robot.curve(radius=600, angle=90)
