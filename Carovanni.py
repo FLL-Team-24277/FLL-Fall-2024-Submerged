d727266563f1c2eaea683c3d825121f6165429b1
Gio <gsykes2030@norfolkcollegiate.org>
2024-09-25 15:18:39 -0400

    Push

---
+++ Carovanni.py
@@ -0,0 +1,28 @@
+from base_robot import *
+
+# Add good comments, such as what the mission is supposed to do,
+# how to align the robot in home, any initial starting instructions,
+# such as how it should be loaded with anything, arm positions, etc.
+# When we run this program from the master program, we will call this
+# "Run(br)" method.
+
+
+def Run(br: BaseRobot):
+    br.driveForDistance(250)
+    br.moveRightAttachmentMotorForDegrees(500)
+    br
+    
+    # Your mission code goes here, step-by-step
+    # It MUST be indented just like the lines below
+
+
+
+# If running this program directly (not from the master program), this is
+# how we know it is running directly. In which case, this method will
+# create a BaseRobot and run the Run(br) method above.
+# In other words, keep these three lines at the bottom of your code and
+# everything will be fine.
+if __name__ == "__main__":
+    br = BaseRobot()
+    Run(br)
+    
