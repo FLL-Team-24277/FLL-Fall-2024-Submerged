e760ebda3ae14be7374ea065300f1dc6505b8563
noah <nbernstein2031@norfolkcollegiate.org>
2024-10-08 16:53:09 -0400

    nextstep

--- noah2.py
+++ noah2.py
@@ -11,25 +11,25 @@ def Run(br: BaseRobot):
     # Your mission code goes here, step-by-step
     # It MUST be indented just like the lines below
 
-    br.driveForDistance(480, speedPct=100, then=Stop.NONE)
-    br.driveArcDist(radius=255, dist=280)
-    # br.driveForDistance(30)
-    # br.moveLeftAttachmentMotorForMillis(millis=1000, speedPct=-100, wait=False)
-    # br.moveLeftAttachmentMotorForDegrees(400)
-    # br.moveRightAttachmentMotorForMillis(millis=750, speedPct=-75)
-    # br.moveRightAttachmentMotorForMillis(millis=250, speedPct=10)
+    br.driveForDistance(500, speedPct=100, then=Stop.NONE)
+    br.driveArcDist(radius=320, dist=200, then=Stop.NONE)
+    br.driveForDistance(125)
+    br.moveLeftAttachmentMotorForMillis(millis=1000, speedPct=-100, wait=False)
+    br.moveLeftAttachmentMotorForDegrees(400)
+    br.moveRightAttachmentMotorForMillis(millis=750, speedPct=-75)
+    br.moveRightAttachmentMotorForMillis(millis=250, speedPct=10)
     # WIGGLE WIGGLE
 
     # br.moveLeftAttachmentMotorForMillis(millis=250, speedPct=-100)
     # br.moveLeftAttachmentMotorForMillis(millis=250, speedPct=100)
     # br.moveLeftAttachmentMotorForMillis(millis=250, speedPct=-100)
-    # br.moveLeftAttachmentMotorForMillis(millis=500, speedPct=-100, wait=False)
+    br.moveLeftAttachmentMotorForMillis(millis=500, speedPct=-100, wait=False)
     # END OF WIGGLE WIGGLE
 
-    # br.waitForMillis(500)
-    # br.driveForDistance(-300)
-    # br.turnInPlace(-80)
-    # br.driveForDistance(-750)
+    br.waitForMillis(500)
+    br.driveForDistance(-300)
+    br.turnInPlace(-80)
+    br.driveForDistance(-750)
 
 
 # If running this program directly (not from the master program), this is
