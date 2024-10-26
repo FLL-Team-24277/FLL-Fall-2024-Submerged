3a4723f2ba2f151cd6094b30375f714acf679462
Gio <gsykes2030@norfolkcollegiate.org>
2024-10-26 12:50:55 -0400

    end practice 10/26/24 gio

--- Gio.py
+++ Gio.py
@@ -13,11 +13,22 @@ def Run(br: BaseRobot):
     # It MUST be indented just like the lines below
     
     #br.driveForDistance(600)
-    br.driveForDistance(1000)
+    br.driveForDistance(400, then=Stop.NONE)
+    br.driveArcDist(radius=800, dist=180, then=Stop.NONE)
+    br.driveArcDist(radius=-800, dist=180, then=Stop.NONE)
+    br.driveForDistance(300)
     br.moveRightAttachmentMotorForDegrees(-75)
     br.driveForDistance(-180)
-    br.moveRightAttachmentMotorForDegrees(25)
-    br.driveArcDist(radius=-340, dist=-150, speedPct=100, then=Stop.NONE)
+    br.moveRightAttachmentMotorForDegrees(95)
+    br.driveForDistance(-100)
+    br.moveRightAttachmentMotorForDegrees(75)
+    br.driveForDistance(50)
+    br.moveRightAttachmentMotorForDegrees(90)
+    br.turnInPlace(-40)
+    br.driveForDistance(350)
+    br.turnInPlace(35)
+    br.driveForDistance(200)
+
     # If running this pogram directly (not from the master program), this is
 # how we know it is running directly. In which case, this method will
 # create a BaseRobot and run the Run(br) method above.
