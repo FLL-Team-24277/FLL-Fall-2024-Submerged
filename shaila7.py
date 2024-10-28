baa3cf93d82ad0a755992191174efc2773c50a95
sbeaufort2031@norfolkcollegiate.org <sbeaufort2031@norfolkcollegiate.org>
2024-10-28 16:38:01 -0400

    im tired

--- shaila7.py
+++ shaila7.py
@@ -3,4 +3,14 @@ from base_robot import BaseRobot
 
 def Run(br: BaseRobot):
 
-    br.driveForDistance(-30)
+    br.driveForDistance(-65, speedPct=67)
+
+    br.driveForDistance(215)
+    br.waitForForwardButton()
+    br.driveForDistance(300)
+
+
+# everything will be fine.
+if __name__ == "__main__":
+    br = BaseRobot()
+    Run(br)
