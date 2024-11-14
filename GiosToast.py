f312439279fc5bcd75e1bf727ef3326a767a06d4
Gio <gsykes2030@norfolkcollegiate.org>
2024-11-14 17:31:51 -0500

    helloo

--- GiosToast.py
+++ GiosToast.py
@@ -1,21 +1,22 @@
 from base_robot import *
 
-# Add good comments, such as what the mission is supposed to do,
+# Add    good comments, such as what the mission is supposed to do,
 # how to align the robot in home, any initial starting instructions,
 # such as how it should be loaded with anything, arm positions, etc.
 # When we run this program from the master program, we will call this
-#"Run(br)" method.
+    #"Run(br)" method.
 def Run(br: BaseRobot):
     # Your mission code goes here, step-by-step
  
     # It MUST be indented just like the lines below
     
-    br.driveForDistance(400, then=Stop.NONE)
-    br.driveArcDist(radius=800, dist=180, then=Stop.NONE)
+    br.driveForDistance(345, then=Stop.NONE)
+    br.driveArcDist(radius=750, dist=180, then=Stop.NONE)
     br.driveArcDist(radius=-800, dist=180, then=Stop.NONE)
     br.driveForDistance(300)
     br.moveRightAttachmentMotorForDegrees(-75)
     br.driveForDistance(-180)
+    br.moveRightAttachmentMotorForDegrees(100)
     br.moveRightAttachmentMotorForDegrees(100)
     br.driveForDistance(-50)   
     br.turnInPlace(-35)
@@ -27,6 +28,7 @@ def Run(br: BaseRobot):
     br.driveForDistance(-400)
     br.driveArcDist(radius=-200, dist=475, then=Stop.NONE)
     br.driveForDistance(500,100)
+
 
 
 
