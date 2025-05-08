02b3f2e85ac646fd1c329b02a4d01d6382211e96
noah <nbernstein2031@norfolkcollegiate.org>
2025-05-08 16:50:50 -0400

    blah blah bloo

--- shaila2.py
+++ shaila2.py
@@ -6,9 +6,9 @@ def Run(br: BaseRobot):
     br.driveForDistance(-65, speedPct=67)
     br.driveForDistance(115)
     br.waitForForwardButton()
-    br.driveForDistance(distance=430, speedPct=25, then=Stop.NONE)
+    br.driveForDistance(distance=350, speedPct=25, then=Stop.NONE)
     br.driveArcDist(radius=650, dist=780, speedPct=45, gyro=False)
-    br.driveForDistance(-100)
+    br.driveForDistance(-150, speedPct=100, accelerationPct=100)
 
 
 if __name__ == "__main__":
